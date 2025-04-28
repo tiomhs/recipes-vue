c04371b7bc78ba725fd5f1e98a9695c8fef01747
Tio Mahesa <tiomahesa.work@gmail.com>
2025-04-28 18:01:31 +0800

    UPDATE - MAKE COMPONENT

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,19 @@
    import { type RecipeResponse } from "../../types/types";
 
     const { data, error } = await useFetch<RecipeResponse>("https://dummyjson.com/recipes?limit=12");
+
+useSeoMeta({
+  title: "Nuxtcipes",
+  description: "Recipes for you to cook!",
+  ogTitle: "Nuxtcipes",
+  ogDescription: "Recipes for you to cook!",
+  ogImage: "/nuxt-course-hero.png",
+  ogUrl: `http:localhost:3000`,
+  twitterTitle: "Nuxtcipes",
+  twitterDescription: "Recipes for you to cook!",
+  twitterImage: "nuxt-course-hero.png",
+  twitterCard: "summary",
+});
 </script>
 
 <template>
@@ -31,34 +44,10 @@
     <section class="py-20 container">
       <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
       <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes!</p>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
-        <div v-for="recipe in data?.recipes" class="flex flex-col shadow rounded-md">
-          <NuxtImg :src="recipe.image" alt="" class="rounded-t-md" />
-          <div class="flex flex-col py-6 px-4 flex-1">
-            <p class="text-xl lg:text-2xl font-semibold mb-2">{{ recipe.name }}</p>
-            <div class="font-normal w-full bg-white/80 flex gap-8 text-lg lg:text-xl mb-4 mt-auto">
-              <div class="flex items-center gap-1">
-                <Icon name="mdi:clock-time-eight-outline" style="color: #f79f1a" />
-                <span>{{ recipe.cookTimeMinutes }}</span>
-              </div>
-              <div class="flex items-center gap-1">
-                <Icon name="mdi:fire" style="color: #f79f1a" />
-                <span>{{ recipe.caloriesPerServing }}</span>
-              </div>
-              <div class="flex items-center gap-1">
-                <Icon name="mdi:star" style="color: #f79f1a" />
-                <span>{{ recipe.reviewCount }}</span>
-              </div>
-            </div>
-            <NuxtLink
-                :to="`/recipes/${recipe.id}`"
-              class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
-            >
-              View
-            </NuxtLink>
-          </div>
-        </div>
+      <div v-if="!error" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+        <RecipeCard v-for="recipe in data?.recipes" :recipe="recipe" />
       </div>
+      <p v-else class="text-xl">Opps, something went wrong, Please try again Later</p>
     </section>
    </main>
 </template>
