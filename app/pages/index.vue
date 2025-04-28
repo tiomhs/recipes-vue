f3ce3b65ebfc952e426d97f6cfa7931885725e03
Tio Mahesa <tiomahesa.work@gmail.com>
2025-04-28 14:30:10 +0800

    commit 4 - error page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -50,11 +50,12 @@
                 <span>{{ recipe.reviewCount }}</span>
               </div>
             </div>
-            <button
+            <NuxtLink
+                :to="`/recipes/${recipe.id}`"
               class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
             >
               View
-            </button>
+            </NuxtLink>
           </div>
         </div>
       </div>
