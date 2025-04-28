4a307c2ebb418b8c3e9fae501ecd2d051383e296
Tio Mahesa <tiomahesa.work@gmail.com>
2025-04-28 13:36:12 +0800

    commit 4 - consume api

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -5,14 +5,14 @@
 <template>
     <header class="py-4 border-b">
         <nav class="flex container items-center">
-            <a href="" class="flex gap-1 items-center">
-                <img src="/icon-green.png" alt="" class="w-14" />
+            <NuxtLink to="/" class="flex gap-1 items-center">
+                <NuxtImg src="/icon-green.png" alt="" class="w-14" />
                 <span class="text-3xl font-bold">Nuxtcipes</span>
-            </a>
+            </NuxtLink>
             <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
-                <li><a href="/" class="text-gray-500 hover:text-gray-900">Home</a></li>
-                <!-- <li><a href="" class="text-gray-500 hover:text-gray-900">About</a></li>
-                <li><a href="" class="text-gray-500 hover:text-gray-900">Contact</a></li> -->
+                <li><NuxtLink to="/" class="text-gray-500 hover:text-gray-900">Home</NuxtLink></li>
+                <li><NuxtLink to="/about" class="text-gray-500 hover:text-gray-900">About</NuxtLink></li>
+                <!-- <li><a href="" class="text-gray-500 hover:text-gray-900">Contact</a></li> -->
 
             </ul>
         </nav>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,10 @@ export default defineNuxtConfig({
       }
     },
 
+    image: {
+      domains: ['https://dummyjson.com'],
+    },
+
   devtools: { enabled: true },
 
   modules: [
